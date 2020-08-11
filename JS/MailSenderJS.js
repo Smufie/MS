7f1df09782138fabe00579b6b3db7c4902468ea6
Jan Olszewski <janolszew@o2.pl>
2020-08-11 10:26:13 +0200

    First commit.

--- JS/MailSenderJS.js
+++ JS/MailSenderJS.js
@@ -1,2 +1,28 @@
+var menuButton = window.document.getElementById("menu-link-4");
 var mainSectionElement = window.document.getElementById("main-section");
-mainSectionElement.classList.add("red");
+var classChanged = false;
+var header = window.document.getElementById("main-header");
+var headerBlinked = false;
+//debugger;
+menuButton.onclick = function() {
+    if (!classChanged) {
+        mainSectionElement.classList.add("red");
+        classChanged = !classChanged;
+    } else {
+        mainSectionElement.classList.remove("red");
+        classChanged = !classChanged;
+    }
+
+    
+    function blink() {
+        if(!headerBlinked) {
+            headerBlinked = true;
+            header.classList.add("blinker");
+            setTimeout(blink(), 1000);
+        } else {
+            headerBlinked = false;
+            header.classList.remove("blinker");
+            setTimeout(blink(), 1000);
+        }
+    }
+};
