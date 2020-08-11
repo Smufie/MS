f86ea7b4e345802c21656feb49b1d603cd16d7fc
Jan Olszewski <janolszew@o2.pl>
2020-08-11 13:58:23 +0200

    Aktualizacja README, animacja headera, dodatkowe funkcje buttona.

--- JS/MailSenderJS.js
+++ JS/MailSenderJS.js
@@ -1,28 +1,19 @@
-var menuButton = window.document.getElementById("menu-link-4");
-var mainSectionElement = window.document.getElementById("main-section");
+var menuButton4 = document.getElementById("menu-link-4");
+var mainSectionElement = document.getElementById("main-section");
 var classChanged = false;
-var header = window.document.getElementById("main-header");
+var header = document.getElementById("main-header");
 var headerBlinked = false;
 //debugger;
-menuButton.onclick = function() {
+menuButton4.onclick = function() {
     if (!classChanged) {
         mainSectionElement.classList.add("red");
+        document.getElementsByClassName("article-in-main-section")[2].innerHTML = "<p>I'm changing!</p>";
         classChanged = !classChanged;
     } else {
         mainSectionElement.classList.remove("red");
+        document.getElementsByClassName("article-in-main-section")[2].innerHTML =
+        "<p>This is column #3 in main section.</p>";
         classChanged = !classChanged;
     }
 
-    
-    function blink() {
-        if(!headerBlinked) {
-            headerBlinked = true;
-            header.classList.add("blinker");
-            setTimeout(blink(), 1000);
-        } else {
-            headerBlinked = false;
-            header.classList.remove("blinker");
-            setTimeout(blink(), 1000);
-        }
-    }
 };
